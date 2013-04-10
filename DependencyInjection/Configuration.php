8738217caef5e3b18f5e46909ff496bb163bfcd5
Daniel Brooks <daniel.brooks@curveu.com>
2013-04-09 18:23:11 -0700

    Fixing config bugs

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -5,7 +5,7 @@ namespace DABSquared\PushNotificationsBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-class Configuration implements ConfigurationInterface
+class Configuration
 {
     /**
      * @var \Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition
@@ -15,35 +15,50 @@ class Configuration implements ConfigurationInterface
     /**
      * Generates the configuration tree builder.
      *
-     * @return \Symfony\Component\Config\Definition\Builder\TreeBuilder The tree builder
+     * @return NodeInterface
      */
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $this->root = $treeBuilder->root("dabsquared_push_notifications");
+        $this->root = $treeBuilder->root("dab_push_notifications");
 
 
         $this->root
             ->children()
-                ->scalarNode('db_driver')->cannotBeOverwritten()->isRequired()->end()
-                ->scalarNode('model_manager_name')->defaultNull()->end()
+            ->scalarNode('db_driver')->cannotBeOverwritten()->isRequired()->end()
+            ->scalarNode('model_manager_name')->defaultNull()->end()
 
-                ->arrayNode('class')->isRequired()
-                    ->children()
-                        ->arrayNode('model')->isRequired()
-                            ->children()
-                                ->scalarNode('device')->isRequired()->end()
-                                ->scalarNode('message')->isRequired()->end()
-                            ->end()
-                        ->end()
-                    ->end()
-                ->end();
+            ->arrayNode('class')->isRequired()
+            ->children()
+            ->arrayNode('model')->isRequired()
+            ->children()
+            ->scalarNode('device')->isRequired()->end()
+            ->scalarNode('message')->isRequired()->end()
+            ->end()
+            ->end()
+            ->end()
+            ->end()
+
+            ->arrayNode('service')->addDefaultsIfNotSet()
+            ->children()
+            ->arrayNode('manager')->addDefaultsIfNotSet()
+            ->children()
+            ->scalarNode('device')->cannotBeEmpty()->defaultValue('dab_push_notifications.manager.device.default')->end()
+            ->scalarNode('message')->cannotBeEmpty()->defaultValue('dab_push_notifications.manager.message.default')->end()
+            ->end()
+            ->end()
+            ->end()
+            ->end()
+
+            ->end();
 
         $this->addAndroid();
         $this->addiOS();
         $this->addBlackberry();
 
-        return $treeBuilder;
+
+
+        return $treeBuilder->buildTree();
     }
 
     /**
@@ -53,34 +68,34 @@ class Configuration implements ConfigurationInterface
     {
         $this->root->
             children()->
-                arrayNode("android")->
-                    canBeUnset()->
-                    children()->
+            arrayNode("android")->
+            canBeUnset()->
+            children()->
 
-                        // WARNING: These 3 fields as they are, outside of the c2dm array
-                        // are deprecrated in favour of using the c2dm array configuration
-                        // At present these will be overriden by anything supplied
-                        // in the c2dm array
-                        scalarNode("username")->defaultValue("")->end()->
-                        scalarNode("password")->defaultValue("")->end()->
-                        scalarNode("source")->defaultValue("")->end()->
+            // WARNING: These 3 fields as they are, outside of the c2dm array
+            // are deprecrated in favour of using the c2dm array configuration
+            // At present these will be overriden by anything supplied
+            // in the c2dm array
+            scalarNode("username")->defaultValue("")->end()->
+            scalarNode("password")->defaultValue("")->end()->
+            scalarNode("source")->defaultValue("")->end()->
 
-                        arrayNode("c2dm")->
-                            canBeUnset()->
-                            children()->
-                                scalarNode("username")->isRequired()->end()->
-                                scalarNode("password")->isRequired()->end()->
-                                scalarNode("source")->defaultValue("")->end()->
-                            end()->
-                        end()->
-                        arrayNode("gcm")->
-                            canBeUnset()->
-                            children()->
-                                scalarNode("api_key")->isRequired()->cannotBeEmpty()->end()->
-                            end()->
-                        end()->
-                    end()->
-                end()->
+            arrayNode("c2dm")->
+            canBeUnset()->
+            children()->
+            scalarNode("username")->isRequired()->end()->
+            scalarNode("password")->isRequired()->end()->
+            scalarNode("source")->defaultValue("")->end()->
+            end()->
+            end()->
+            arrayNode("gcm")->
+            canBeUnset()->
+            children()->
+            scalarNode("api_key")->isRequired()->cannotBeEmpty()->end()->
+            end()->
+            end()->
+            end()->
+            end()->
             end()
         ;
     }
@@ -92,14 +107,14 @@ class Configuration implements ConfigurationInterface
     {
         $this->root->
             children()->
-                arrayNode("ios")->
-                    children()->
-                        booleanNode("sandbox")->defaultFalse()->end()->
-                        scalarNode("pem")->isRequired()->cannotBeEmpty()->end()->
-                        scalarNode("passphrase")->defaultValue("")->end()->
-                        scalarNode('json_unescaped_unicode')->defaultFalse()->info('PHP >= 5.4.0 and each messaged must be UTF-8 encoding')->end()->
-                    end()->
-                end()->
+            arrayNode("ios")->
+            children()->
+            booleanNode("sandbox")->defaultFalse()->end()->
+            scalarNode("pem")->isRequired()->cannotBeEmpty()->end()->
+            scalarNode("passphrase")->defaultValue("")->end()->
+            scalarNode('json_unescaped_unicode')->defaultFalse()->info('PHP >= 5.4.0 and each messaged must be UTF-8 encoding')->end()->
+            end()->
+            end()->
             end()
         ;
     }
@@ -111,13 +126,13 @@ class Configuration implements ConfigurationInterface
     {
         $this->root->
             children()->
-                arrayNode("blackberry")->
-                    children()->
-                        booleanNode("evaluation")->defaultFalse()->end()->
-                        scalarNode("app_id")->isRequired()->cannotBeEmpty()->end()->
-                        scalarNode("password")->isRequired()->cannotBeEmpty()->end()->
-                    end()->
-                end()->
+            arrayNode("blackberry")->
+            children()->
+            booleanNode("evaluation")->defaultFalse()->end()->
+            scalarNode("app_id")->isRequired()->cannotBeEmpty()->end()->
+            scalarNode("password")->isRequired()->cannotBeEmpty()->end()->
+            end()->
+            end()->
             end()
         ;
     }
